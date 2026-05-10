d823f21ed322170d9d0ff22db950875749a69bb0
syugo0630-ai <syu.go0630@gmail.com>
2026-05-10 19:54:22 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+const CACHE_NAME='daily-plan-v2';const ASSETS=['./index.html','./data.js','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
